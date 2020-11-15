9542a7675b097d439d969d1403668643538fae8c
coloneldev <73738792+coloneldev@users.noreply.github.com>
2020-11-15 18:14:15 +0100

    Add files via upload

---
+++ exemple_de_commande.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+exports.run = function (client, message) { // Le script à exécuter
+
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [' '], //Autres façons d'appeler la commande
+    permLevel: 0
+};
+  
+  exports.help = { //Si l'utilisateur tape  ;help <Commande>
+    name: '',
+    description: ' ',
+    usage: ''
+};
